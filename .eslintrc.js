23bdcd399e436765e43f6272209ec499d704dccc
Alex Martin <alextravc.342@gmail.com>
2021-12-16 22:06:57 -0500

    [ADD]: initial project structure

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    commonjs: true,
+    jest: true,
+    es6: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    semi: ['error', 'always'],
+  },
+};
---
+++ README.md
@@ -0,0 +1,27 @@
+# TypeScript Express API Bootstrap (base / project starter)
+
+This is a repository intended to serve as a starting point if you want to bootstrap a express API project in TypeScript.
+
+## Running the app
+
+```
+# install dependencies
+npm install
+
+# run in dev mode on port 3000
+npm run dev
+
+# generate production build
+npm run build
+
+# run generated content in dist folder on port 3000
+npm run start
+```
+
+## Testing
+
+### Jest with supertest
+
+```
+npm run test
+```
