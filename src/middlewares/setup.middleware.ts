9a9a319562348a6500aa3272b98829afe1653c04
Adrian Changalombo <adrian.changalombo45@gmail.com>
2022-01-17 15:24:29 -0500

    [FIX]: npm i basic-setup & setup middlewares

--- src/middlewares/setup.middleware.ts
+++ src/middlewares/setup.middleware.ts
@@ -4,11 +4,14 @@ import cors from 'cors';
 import helmet from 'helmet';
 import compression from 'compression';
 import morgan from 'morgan';
+import path from 'path';
 
 export const setupMiddlewares = (app: Application) => {
+  app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
-  app.use(cors()).use(helmet()).use(compression());
+  app.use(express.static(path.join(__dirname, './../public')));
+  app.use(compression()).use(helmet());
   app.use(morgan('dev'));
 
   // Other middlewares
