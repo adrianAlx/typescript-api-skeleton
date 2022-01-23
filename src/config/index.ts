8461c3feb8e398c8e95f21833afa584db99e663b
Adrian Changalombo <adrian.changalombo45@gmail.com>
2022-01-22 22:11:08 -0500

    [FIX]: package.js - scripts

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,5 +2,5 @@ import { config } from 'dotenv';
 
 if (process.env.NODE_ENV !== 'production') config();
 
-export const PORT = process.env.PORT;
-export const MONGODB_URI = process.env.MONGODB_URI;
+export const PORT = `${process.env.PORT}`;
+export const MONGODB_URI = `${process.env.MONGODB_URI}`;
